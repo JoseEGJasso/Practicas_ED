8936f7b2bd41b0aba6aa9dee27a24024742ce181
JoseEGJasso <josejasso2000@ciencias.unam.mx>
2019-03-17 12:49:15 -0600

    Practica4_Subo Ordenamientos incompleto

--- src/Ordenamientos.java
+++ src/Ordenamientos.java
@@ -33,14 +33,13 @@ public class Ordenamientos{
         return i;
     }
 
-    //regresa una lista
     public static <T extends Comparable<T>> Lista<T> mergeSort(Lista<T> l){
 
       Lista<T> l1 = new Lista<T>();
       Lista<T> l2 = new Lista<T>();
 
       if(l.getLongitud() == 0 || l.getLongitud() == 1)
-           return l.copia();
+        return l.copia();
 
 
      for(int i=0;i<l.getLongitud()/2;i++){
@@ -130,6 +129,21 @@ public class Ordenamientos{
     }
 
     public static <T extends Comparable<T>> void bubbleSort(T[] a){
+        int longitud=a.length;
+
+        for(int j=0;j<a.length;j++){
+            for(int i=0;i<longitud;i++){
+                if(i==a.length-1)
+                    break;
+                if(a[i].compareTo(a[i+1])>=0){
+                    T aux=(T) a[i];
+
+                    a[i]=a[i+1];
+                    a[i+1]=aux;
+                }
+            }
+            longitud--;
+        }
     }
 
     public static <T extends Comparable<T>> void selectionSort(T[] a){
@@ -147,19 +161,126 @@ public class Ordenamientos{
             aux=(T)a[min];
             a[min]=a[j];
             a[j]=aux;
-
         }
+    }
+
+    public static int numerosIntRandom(){
+
+        return (int)(Math.random()*999999);
+    }
+
+
+    /**
+     * Método que calcula el tiempo de ejecución del algoritmo
+     * Selection Sort. Si la longitud del arreglo recibido es menor
+     * o igual a 10000, calcula el tiempo de ejecución en milisegundos,
+     * en caso contrario en segundos.
+     * @param a; arreglo a ordenar
+     * @return long; representa el tiempo de ejecución calculado
+     */
+    public static <T extends Comparable<T>> long tiempoSelectionSort(T[] a){
+        long ahora=System.currentTimeMillis();
+
+        selectionSort(a);
+
+        long despues=System.currentTimeMillis();        
+
+        if(a.length<=10000)
+            return (despues-ahora);
+
+        return (despues-ahora)/1000;
+    }
+
+
+    /**
+     * Método que calcula el tiempo de ejecución del algoritmo
+     * Bubble Sort. Si la longitud del arreglo recibido es menor
+     * o igual a 10000, calcula el tiempo de ejecución en milisegundos,
+     * en caso contrario en segundos.
+     * @param a; arreglo a ordenar
+     * @return long; representa el tiempo de ejecución calculado     
+     */
+    public static <T extends Comparable<T>> long tiempoBubbleSort(T[] a){
+        long ahora=System.currentTimeMillis();
+
+        bubbleSort(a);
+
+        long despues=System.currentTimeMillis();
+
+        if(a.length<=10000)
+            return (despues-ahora);
+
+        return (despues-ahora)/1000;
+    }
+
+
+    public static <T extends Comparable<T>> long tiempoQuickSort(T[] a){
+        long ahora=System.currentTimeMillis();
+
+        quickSort(a);
+
+        long despues=System.currentTimeMillis();
+
+        if(a.length<=10000)
+            return (despues-ahora);
+
+        return (despues-ahora);
+    }
+
+
+    private static Integer[] generaArreglosInteger(int n){
+        Integer[] arreglo=new Integer[n];
+
+        for (int i=0;i<arreglo.length;i++) {
+            arreglo[i]=numerosIntRandom();
+        }
+
+        return arreglo;
     }
 
     public static void main(String args[]){
         Integer[] arreglo={6,1,5,10,9,2,2,2,8,7};
 
-        Ordenamientos.quickSort(arreglo,0,arreglo.length);
 
-       // for (Integer e:arreglo) {
-         //   System.out.print(e+",");
-       // }
+        System.out.println("SELECTION SORT.(tiempo calculado en segundos para # elementos > 10000 y en milisegundos para <=10000)");
 
-        System.out.println(Ordenamientos.busquedaBinaria(arreglo,7));
+        System.out.println("Tiempo de ejecución con 100 elementos: "+Ordenamientos.tiempoSelectionSort(Ordenamientos.generaArreglosInteger(100)));
+
+        System.out.println("Tiempo de ejecución con 1000 elementos: "+Ordenamientos.tiempoSelectionSort(Ordenamientos.generaArreglosInteger(1000)));
+
+        System.out.println("Tiempo de ejecución con 10000 elementos: "+Ordenamientos.tiempoSelectionSort(Ordenamientos.generaArreglosInteger(10000)));        
+        
+        //System.out.println("Tiempo de ejecución con 100000 elementos: "+Ordenamientos.tiempoSelectionSort(Ordenamientos.generaArreglosInteger(100000)));
+
+        //RECOMENDACION: No ejecutar el tiempo de ejecución con 300000 elementos, llega a durar más de 10 min.
+        //System.out.println("Tiempo de ejecución con 300000 elementos: "+Ordenamientos.tiempoSelectionSort(Ordenamientos.generaArreglosInteger(300000)));
+
+        System.out.println("BUBBLE SORT.(tiempo calculado en segundos para # elementos > 10000 y en milisegundos para <=10000");
+
+        System.out.println("Tiempo de ejecución con 100 elementos: "+Ordenamientos.tiempoBubbleSort(Ordenamientos.generaArreglosInteger(100)));
+
+        System.out.println("Tiempo de ejecución con 1000 elementos: "+Ordenamientos.tiempoBubbleSort(Ordenamientos.generaArreglosInteger(1000)));
+
+        System.out.println("Tiempo de ejecución con 10000 elementos: "+Ordenamientos.tiempoBubbleSort(Ordenamientos.generaArreglosInteger(10000)));           
+        
+        //RECOMENDACION: No ejecutar la siguiente linea, pues llega a tardar hasta 20 min.
+        //System.out.println("Tiempo de ejecución con 100000 elementos: "+Ordenamientos.tiempoBubbleSort(Ordenamientos.generaArreglosInteger(100000)));
+
+        //RECOMENDACION: No ejecutar la siguiente linea, pues llega a tardar hasta 18 horas.
+        //System.out.println("Tiempo de ejecución con 300000 elementos: "+Ordenamientos.tiempoBubbleSort(Ordenamientos.generaArreglosInteger(300000)));
+
+        System.out.println("QUICK SORT.(tiempo calculado en segundos para # elementos > 10000 y en milisegundos para <=10000");
+
+        System.out.println("Tiempo de ejecución con 100 elementos: "+Ordenamientos.tiempoQuickSort(Ordenamientos.generaArreglosInteger(1000)));
+
+        System.out.println("Tiempo de ejecución con 1000 elementos: "+Ordenamientos.tiempoQuickSort(Ordenamientos.generaArreglosInteger(1000)));
+
+        System.out.println("Tiempo de ejecución con 10000 elementos: "+Ordenamientos.tiempoQuickSort(Ordenamientos.generaArreglosInteger(10000)));        
+
+        System.out.println("Tiempo de ejecución con 100000 elementos: "+Ordenamientos.tiempoQuickSort(Ordenamientos.generaArreglosInteger(100000)));
+
+        System.out.println("Tiempo de ejecución con 300000 elementos: "+Ordenamientos.tiempoQuickSort(Ordenamientos.generaArreglosInteger(300000)));
+
+        //Haz lo mismo con mergeSort y busqueda binaria(crea el método tiempoMergeSort y tiempoBusquedaBinaria, checa los otros para guiarte es casi lo mismo), también comenta lo que falta
     }
 }
