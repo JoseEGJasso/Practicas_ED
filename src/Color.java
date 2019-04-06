8b0b4b611d7d1714cd262da1f78e357dafd6b007
JoseEGJasso <josejasso2000@ciencias.unam.mx>
2019-04-06 17:32:19 -0600

    Practica6_Agrego clase Color

---
+++ src/Color.java
@@ -0,0 +1,14 @@
+public enum Color{
+    ROJO, NEGRO;
+
+    @Override
+    public String toString(){
+	switch(this){
+	case ROJO:
+	    return "Rojo";
+	case NEGRO:
+	    return "Negro";
+	}
+	return null;
+    }
+}
