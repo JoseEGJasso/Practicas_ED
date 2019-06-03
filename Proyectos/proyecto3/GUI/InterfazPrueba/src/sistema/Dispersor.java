ea8c112c794a4b0ad4d018e0abf97e344dcec72a
diegodmag <diegodm@ciencias.unam.mx>
2019-06-02 22:31:53 -0500

    Proyecto_3 Versión FINAL (Revisar)

--- Proyectos/proyecto3/GUI/InterfazPrueba/src/sistema/Dispersor.java
+++ Proyectos/proyecto3/GUI/InterfazPrueba/src/sistema/Dispersor.java
@@ -2,9 +2,11 @@ package sistema;
 
 /**
  * Interfaz funcional para implementar funciones de dispersión con lambdas.
+ * @author González Jasso José Eduardo
+ * @author Dozal Magnani Diego
  */
 @FunctionalInterface
 public interface Dispersor<K>{
 
-    int dispersa(K elemento);    
+    int dispersa(K elemento);
 }
